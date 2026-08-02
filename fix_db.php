e40b2a3d6598de34bb705ff9de1831ba2e88d6f6
Prashant Karn <karnprashant70@gmail.com>
2026-08-02 18:42:18 +0545

    refactor: remove unused test scripts, debug log files, and temporary seed files

--- fix_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'db-connection/db_conn.php';
-$conn->query("DROP TABLE IF EXISTS tbl_doctor");
-echo "Dropped tbl_doctor\n";
-require 'db-connection/db_conn.php';
-echo "Recreated tbl_doctor\n";
